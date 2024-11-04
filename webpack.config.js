d66c2eb394ec5029b23578d9c8a07fdf3efe4dc3
António Rebelo <imtoninho@gmail.com>
2024-11-04 19:47:35 +0000

    ?

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
     }),
     new CopyWebpackPlugin({
       patterns: [
-        { from: path.resolve(__dirname, 'src/assets'), to: 'assets' },
+        { from: path.resolve(__dirname, '/assets'), to: 'assets' },
       ],
     }),
   ],
